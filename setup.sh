c2a7eb4c5b8d8463e45a953b05cedd6b632be6fd
ykm11 <ushiromiya3@gmail.com>
2018-07-17 17:14:47 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,24 @@
 
 DOT_FILES=(.zshrc .vimrc)
 
+echo "[+] Pasting symblic Link\n"
 for file in ${DOT_FILES[@]}
 do
     ln -s ./$file $HOME/$file
 done
+echo "[+] Done"
 
+
+echo "[+] Setting for Neovim\n"
 if [ -e $HOME/.config/nvim ]; then
     ln -s ./init.vim $HOME/.config/nvim/init.vim
-    echo "\nINSTALL dein.vim -> https://github.com/Shougo/dein.vim\n"
+    echo "\nInstall dein.vim -> https://github.com/Shougo/dein.vim\n"
 
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
     sh ./installer.sh ~/.cache/dein
 
-    echo "INSTALL molokai -> https://github.com/tomasr/molokai"
+    echo "Install molokai -> https://github.com/tomasr/molokai"
 else
-    echo "NAI"
-    # Install Neovim
+    echo "Install Neovim -> https://github.com/neovim/neovim"
 fi
+echo "[+] Done"
